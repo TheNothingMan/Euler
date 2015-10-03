e8e4fcf8d6cffcaaeabf155f57581b7fce3274b9
S. Käppler <s.skr.k123@gmail.com>
2015-10-03 15:22:14 +0200

    Updated 35
    On the way...

--- Euler35.py
+++ Euler35.py
@@ -1,21 +1,56 @@
 import time
+import math
+import itertools
 
-print("Euler 34 - circular primes")
+print("Euler 35 - circular primes")
 
 e="y"
 
 while e == "y":
-  primes = [2,3,5,7,11,13,17]
-  end = int(input("Enter range --> "))
-  for i in range (23, end):
-    k = 0
-    check = True
-    while primes[k] < i//2 and check:
-      if i%primes[k]==0:
-        check = False
-      k += 1
-    if check:
-      primes.append(i)
-  
-  print(primes)
-  e = input("Again? y/n --> ")
+	primes = [2,3,5,7,11,13,17,19]
+	end = int(input("Enter range --> "))
+	# old = time.time()
+	# for i in range (23, end, 2):
+		# k = 0
+		# check = True
+		# while primes[k] <= math.sqrt(i) and check:
+			# if i%primes[k]==0:
+				# check = False
+			# k += 1
+		# if check:
+			# primes.append(i)
+	
+	# print(primes, time.time()-old)
+	
+	old = time.time()
+	primes_h = [1]*(end+2)
+	for i in range(2,int(math.sqrt(end))+1):
+		k = 2*i
+		while k <= end:
+			primes_h[k] = 0
+			k += i
+	i = 2
+	primes=[]
+	while i<len(primes_h):
+		if primes_h[i]==1:
+			primes.append(i)
+		i+=1
+	print(primes, time.time()-old)
+	count = 0
+
+	test = []
+	for i in primes:
+		check = True
+		permuts = itertools.permutations(list(str(i)))
+		permutints = [int(''.join(x)) for x in permuts]
+		print(permutints)
+		for k in permutints:
+			if k not in primes:
+				check = False
+				break
+		if check:
+			test.append(i)
+			count += 1
+	
+	print(count, test, time.time()-old)
+	e = input("Again? y/n --> ")
